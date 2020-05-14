fd1e641fc7998565dadc3384f88b42747dba23d3
DuldR <gnjulaka@gmail.com>
2020-05-14 17:22:35 -0500

    Need to work on reply portion. Instructions not very clear on what's needed in the database.

--- ORM/Projects/AAQ/import_db.sql
+++ ORM/Projects/AAQ/import_db.sql
@@ -18,6 +18,31 @@ CREATE TABLE questions (
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+DROP TABLE if exists questions_follows;
+
+CREATE TABLE questions_follows
+(
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+
+
+DROP TABLE if exists replies;
+
+CREATE TABLE replies
+(
+  id INTEGER PRIMARY KEY,
+  rtitle TEXT NOT NULL,
+  rbody TEXT NOT NULL,
+  parentreply INTEGER NOT NULL
+
+);
+
 
 INSERT INTO
   users (fname, lname)
@@ -31,23 +56,9 @@ VALUES
   ('Nani', "Omae wa mou, shindeiru", (SELECT id FROM users WHERE fname = 'Dingle' AND lname = 'Dork')),
   ('Funny', "He has a wife you know... Incontinentia Buttocks", (SELECT id FROM users WHERE fname = 'Biggus' AND lname = 'Dickus'));
 
-DROP TABLE if exists question_follows;
-
-CREATE TABLE question_follows AS
-  SELECT
-    questions.body, users.fname, users.lname
-  FROM
-    questions
-  JOIN users
-      ON questions.user_id = users.id;
-
-
-DROP TABLE if exists question_likes;
-
-CREATE TABLE question_likes AS
-  SELECT
-    questions.body, users.fname, users.lname
-  FROM
-    questions
-  JOIN users
-    ON questions.user_id = users.id;
+INSERT INTO
+  questions_follows (user_id, question_id)
+VALUES
+  ((SELECT id FROM users WHERE fname='Dingle' AND lname='Dork'), (SELECT id FROM questions WHERE user_id = 1)),
+  ((SELECT id FROM users WHERE fname='Biggus' AND lname='Dickus'), (SELECT id FROM questions WHERE user_id = 2))
+  ;
