115582541e6d8ca5a33e2f03130f8af434126334
Garrett <gnjulak@gmail.com>
2020-05-22 16:06:58 -0500

    Save method added

--- ORM/Projects/AAQ/questions.rb
+++ ORM/Projects/AAQ/questions.rb
@@ -68,6 +68,33 @@ class User
     QuestionFollow.followed_questions_for_user_id(self.id)
   end
 
+  def save
+
+    if @id == nil
+      QuestionsDatabase.instance.execute(<<-SQL, @fname, @lname)
+
+      INSERT INTO
+        users(fname, lname)
+      VALUES
+        (?, ?)
+      SQL
+
+      @id = QuestionsDatabase.instance.last_insert_row_id
+    else
+      QuestionsDatabase.instance.execute(<<-SQL, @fname, @lname, @id)
+
+      UPDATE
+        users
+      SET
+        fname = ?, lname = ?
+      WHERE
+        id = ?
+      
+      SQL
+    end
+
+  end
+
 end
 
 class Question
