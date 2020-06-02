384071d135f9d01c72c8caf299253fbf1e163dda
DuldR <gnjulaka@gmail.com>
2020-06-01 22:59:41 -0500

    Ensure no duplicate hashes. Also cdded factory method that utilizes create. This automatically saves the new object into the database.

--- ActiveRecord/Projects/URLShortener/app/models/shortened_url.rb
+++ ActiveRecord/Projects/URLShortener/app/models/shortened_url.rb
@@ -1,13 +1,28 @@
 class ShortenedUrl < ApplicationRecord
 
-    validates :name, :short_url, presence: true
+    validates :user_id, :short_url, presence: true
     validates :short_url, uniqueness: true
 
 
     def self.random_code
-        print :short_url
+        not_yet = 0
+        until not_yet == 1
+            dankaku = SecureRandom::urlsafe_base64
+            if self.exists?(:short_url => dankaku) == false
+                not_yet = 1
+            end
+        end
+
+        dankaku
     end
 
+    #This is what they define as a factory method. *shrugs* Doesn't match waht the internet defines as a factory method.
+    def self.create_url(user, long_url)
+        ShortenedUrl.create!(
+            user_id: user.id,
+            long_url: long_url,
+            short_url: ShortenedUrl.random_code
+        )
 
 
 end
