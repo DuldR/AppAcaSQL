a79adabad06ea4dd0f8cce315d113a08cc55ffb4
DuldR <gnjulaka@gmail.com>
2020-07-07 18:04:30 -0500

    Defaults complete.

--- ActUnder/Projects/ActiveRecordLite/lib/03_associatable.rb
+++ ActUnder/Projects/ActiveRecordLite/lib/03_associatable.rb
@@ -53,6 +53,28 @@ end
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     # ...
+
+    name = name.singularize
+    self_class_name = self_class_name.singularize.downcase
+
+
+    if options[:foreign_key].nil?
+      @foreign_key = "#{self_class_name}_id".to_sym
+    else
+      @foreign_key = options[:foreign_key]
+    end
+
+    if options[:class_name].nil?
+      @class_name = name.camelize
+    else
+      @class_name = options[:class_name]
+    end
+
+    if options[:primary_key].nil?
+      @primary_key = :id
+    else
+      @primary_key = options[:primary_key]
+    end
   end
 end
 
