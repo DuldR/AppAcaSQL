6cc80ef5b9fe73f01c57ce003fe6e3e44265379e
DuldR <gnjulaka@gmail.com>
2020-07-10 22:17:11 -0500

    Moved the options definitiation toinside of the define method. This ensures it gets called from inside of an instance and NOT are the class definition.

--- ActUnder/Projects/ActiveRecordLite/lib/04_associatable2.rb
+++ ActUnder/Projects/ActiveRecordLite/lib/04_associatable2.rb
@@ -6,6 +6,33 @@ module Associatable
 
   def has_one_through(name, through_name, source_name)
     # ...
-    
+
+    # You cant do this yet as human hasnt been defined! Do it inside of the define method which will ensure it gets called inside of an instance which relies on the user to ensure the associations are present.
+    # through_options = self.assoc_options[through_name]
+    # source_options = through_options.model_class.assoc_options[source_name]
+
+
+    define_method(name) do
+
+      through_options = self.assoc_options[through_name]source_options = through_options.model_class.assoc_options[source_name]
+
+      fkey = through_options.model_class.assoc_options[source_name].foreign_key
+
+
+      print fkey
+      # data = DBConnection.execute(<<-SQL)
+      #   SELECT
+      #     "#{self.table_name}".*
+      #   FROM
+      #     "#{source_options.model_class.table_name}"
+      #   WHERE
+      #     "#{fkey}" = "#{pkey}"
+      # SQL
+
+      # parse_all(data)
+    end
+
+  
+
   end
 end
